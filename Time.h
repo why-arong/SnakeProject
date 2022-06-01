beae5dd8c0570fa3d28d069e055f51a68a956933
pilmokim <68311908+pilmokim@users.noreply.github.com>
2022-06-01 09:19:38 +0900

    ~~~~~~~~~~~ 시간을 밀리세컨드로 받기위한 헤더파일

---
+++ Time.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <chrono>
+#include "mytime.h"
+#include <time.h>
+
+class Time
+{
+public:
+    static time_t milliseconds()
+    {
+        struct timeval time_now
+        {
+
+        };
+        gettimeofday(&time_now, nullptr);
+        time_t msecs_time = (time_now.tv_sec * 1000) + (time_now.tv_usec / 1000);
+        return msecs_time;
+    }
+};
