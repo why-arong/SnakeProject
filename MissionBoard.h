f7b3dfd1b64b1536009bb03424a10d416413a340
dh5473 <94889715+dh5473@users.noreply.github.com>
2022-06-16 12:42:43 +0900

    Update stage

--- MissionBoard.h
+++ MissionBoard.h
@@ -9,31 +9,38 @@ public:
         mission_win = newwin(12, width - 30, 15, 70);
     }
 
-    void initialize(int current_size, int max_length, int growth_count, int poison_count, int gate_count)
+    void initialize(int stage, int current_size, int max_length, int growth_count, int poison_count, int gate_count)
     {
         clear();
         wborder(mission_win, '|', '|', '-', '-', '+', '+', '+', '+');
         mvwprintw(mission_win, 1, 6, "Mission");
-        mvwprintw(mission_win, 3, 5, "B : %d", 5);
-        mvwprintw(mission_win, 5, 5, "+ : %d", 2);
-        mvwprintw(mission_win, 7, 5, "- : %d", 2);
-        mvwprintw(mission_win, 9, 5, "G : %d", 2);
-        updateGoal(max_length, growth_count, poison_count, gate_count);
+        mvwprintw(mission_win, 3, 5, "B : %d", 4 + stage);
+        mvwprintw(mission_win, 5, 5, "+ : %d", 1 + stage);
+        mvwprintw(mission_win, 7, 5, "- : %d", 1 + stage);
+        mvwprintw(mission_win, 9, 5, "G : %d", 1 + stage);
+        updateGoal(stage, max_length, growth_count, poison_count, gate_count);
         refresh();
     }
-    void updateGoal(int max_length, int growth_count, int poison_count, int gate_count)
+    bool updateGoal(int stage, int max_length, int growth_count, int poison_count, int gate_count)
     {
-        if (max_length < 5)  mvwprintw(mission_win, 3, 12, "(   )");
+        if (max_length < 4+stage)  mvwprintw(mission_win, 3, 12, "(   )");
         else  mvwprintw(mission_win, 3, 12, "( V )");
 
-        if (growth_count < 2)  mvwprintw(mission_win, 5, 12, "(   )");
+        if (growth_count < 1+stage)  mvwprintw(mission_win, 5, 12, "(   )");
         else  mvwprintw(mission_win, 5, 12, "( V )");
 
-        if (poison_count < 2)  mvwprintw(mission_win, 7, 12, "(   )");
+        if (poison_count < 1+stage)  mvwprintw(mission_win, 7, 12, "(   )");
         else  mvwprintw(mission_win, 7, 12, "( V )");
 
-        if (gate_count < 2) mvwprintw(mission_win, 9, 12, "(   )");
+        if (gate_count < 1+stage) mvwprintw(mission_win, 9, 12, "(   )");
         else  mvwprintw(mission_win, 9, 12, "( V )");
+
+        if (max_length >= 4 + stage && growth_count >= 1 + stage &&
+            poison_count >= 1 + stage)
+        {
+            return true;
+        }
+        return false;
     }
 
     void clear()
