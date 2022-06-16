71600f6c7f4a45817295c4278513d25a0d40190d
dh5473 <94889715+dh5473@users.noreply.github.com>
2022-06-16 12:41:53 +0900

    Update stage

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include <curses.h>
 #include "Board.h"
 #include "SnakeGame.h"
-#define BOARDER_DIM 20
+#define BOARDER_DIM 25
 #define BOARDER_ROW BOARDER_DIM
 #define BOARDER_COL BOARDER_DIM * 2
+int stage_num = 1;
 
 int main()
 {
-	
 	initscr();
+	resize_term(30, 110);
 	refresh();
+	curs_set(0);
 
-	noecho(); // 입력해도 윈도우에 보이지 않음.
-
-	SnakeGame game(BOARDER_ROW, BOARDER_COL);
-
-	while (!game.isOver())
+	while (stage_num <= 4)
 	{
-		game.processInput();
-		game.updateState();
-		game.redraw();
+		SnakeGame game(BOARDER_ROW, BOARDER_COL, stage_num);
 
+		while (!(game.isOver() || game.isClear()))
+		{
+			game.processInput();
+			game.updateState();
+			game.redraw();
+		}
+		
+		if (game.isOver()) break;
+		stage_num++;
 	}
 	// 1. get input from user.
 	// 2. update game state.
