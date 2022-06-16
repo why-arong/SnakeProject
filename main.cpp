a7b8578ff954cce8f3261227ff8216543f775ace
dh5473 <94889715+dh5473@users.noreply.github.com>
2022-06-16 18:18:06 +0900

    update lastscreen

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <curses.h>
 #include "Board.h"
 #include "SnakeGame.h"
+#include "AllclearScreen.h"
+#include "GameoverScreen.h"
 #define BOARDER_DIM 25
 #define BOARDER_ROW BOARDER_DIM
 #define BOARDER_COL BOARDER_DIM * 2
 int stage_num = 1;
+boolean clear_game = true;
+AllclearScreen allclear_title;
+GameoverScreen gameover_title;
 
 int main()
 {
@@ -24,15 +29,36 @@ int main()
 			game.redraw();
 		}
 		
-		if (game.isOver()) break;
+		if (game.isOver())
+		{
+			clear_game = false;
+			break;
+		}
 		stage_num++;
 	}
+	
+	if (clear_game)
+	{
+		allclear_title = AllclearScreen();
+		allclear_title.titleOn();
+		allclear_title.titleOff();
+	}
+	else
+	{
+		gameover_title = GameoverScreen();
+		gameover_title.titleOn();
+		gameover_title.titleOff();
+	}
+
 	// 1. get input from user.
 	// 2. update game state.
 	// 3. redraw display.
 	// 4. go to 1 unless game over.
 	getch();
 	endwin();
+
+	return 0;
+}
 
 	return 0;
 }
